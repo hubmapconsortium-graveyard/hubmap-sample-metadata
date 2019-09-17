bdd7ccc572802709af0867e131781cadd8a4a638
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-09-17 12:31:40 -0400

    Use a real test framework; Check rdf against fixture

--- test.py
+++ test.py
@@ -4,15 +4,65 @@ import os
 from pathlib import Path
 import json
 from urllib.parse import urlparse
-from collections import defaultdict
-from pprint import PrettyPrinter
 from io import StringIO
-from textwrap import indent
+import unittest
 
 import wget
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError, SchemaError
 import prov
+
+
+class TestsContainer(unittest.TestCase):
+    hubmap_schema = json.load(open('hubmap-schema.json'))
+
+
+def make_json_test(description, dir_path, name):
+    def test(self):
+        with open(Path(dir_path) / name) as json_fixture:
+            metadata = json.load(json_fixture)
+            expected_suffix = metadata['schema_type'] + '.json'
+            if not name.endswith(expected_suffix):
+                self.fail(f'Expected to end with "{expected_suffix}".')
+            described_by = metadata['describedBy']
+            schema_url = urlparse(described_by)
+            cache_path = f'schema-cache/{schema_url.hostname}{schema_url.path}'
+            if not os.path.isfile(cache_path):
+                download_to(described_by, cache_path)
+            with open(cache_path) as cache_file:
+                schema = json.load(cache_file)
+                try:
+                    validate(instance=metadata, schema=schema)
+                except (ValidationError, SchemaError) as e:
+                    self.fail(e)
+                try:
+                    validate(instance=metadata, schema=self.hubmap_schema)
+                except (ValidationError, SchemaError) as e:
+                    self.fail(e)
+    return test
+
+
+def drop_blank(lines):
+    return set(line for line in lines if line)
+
+
+def make_prov_test(description, dir_path, name):
+    def test(self):
+        rdf_path = Path(dir_path) / name
+        provenance = prov.read(rdf_path, format='rdf')
+        output = StringIO()
+        serializer = prov.serializers.provn.ProvNSerializer(provenance)
+        serializer.serialize(output)
+        actual = output.getvalue()
+        actual_lines = drop_blank(actual.split('\n'))
+
+        with open(Path(dir_path) / 'prov.prov') as prov_fixture:
+            expected_lines = drop_blank(prov_fixture.read().split('\n'))
+            self.assertEqual(
+                actual_lines, expected_lines,
+                msg=f'Copying this to prov.prov might fix the problem:\n{actual}'
+            )
+    return test
 
 
 def download_to(url, target):
@@ -23,62 +73,15 @@ def download_to(url, target):
     os.rename(download_path, target)
 
 
-class TestLogger():
-    def __init__(self):
-        self.hubmap_schema = json.load(open('hubmap-schema.json'))
-        self.fails = defaultdict(dict)
-
-    def validate_json(self, dir_path, name):
-        print(f'\tLoad JSON: {name}')
-        path = Path(dir_path) / name
-        metadata = json.load(open(path))
-        expected_suffix = metadata['schema_type'] + '.json'
-        if not name.endswith(expected_suffix):
-            self.fails[path]['suffix'] = f'Expected to end with "{expected_suffix}".'
-            return
-        described_by = metadata['describedBy']
-        schema_url = urlparse(described_by)
-        cache_path = f'schema-cache/{schema_url.hostname}{schema_url.path}'
-        if not os.path.isfile(cache_path):
-            download_to(described_by, cache_path)
-        schema = json.load(open(cache_path))
-        try:
-            validate(instance=metadata, schema=schema)
-        except (ValidationError, SchemaError) as e:
-            self.fails[path]['hca'] = e
-        try:
-            validate(instance=metadata, schema=self.hubmap_schema)
-        except (ValidationError, SchemaError) as e:
-            self.fails[path]['hubmap'] = e
-
-    def validate_prov(self, dir_path, name):
-        print(f'\tLoad Provenance: {name}')
-        path = Path(dir_path) / name
-        provenance = prov.read(path, format='rdf')
-        output = StringIO()
-        serializer = prov.serializers.provn.ProvNSerializer(provenance)
-        serializer.serialize(output)
-        print(indent(output.getvalue(), '\t'))
-
-
-def main():
-    logger = TestLogger()
+if __name__ == '__main__':
     for dir_path, _, file_names in os.walk('workflows'):
-        print(f'Testing: {dir_path}')
         for name in sorted(file_names):
             if 'TODO' in name:
-                print(f'\tTODO: {name}')
+                print(f'TODO: {name}')
             elif name.endswith('.json'):
-                logger.validate_json(dir_path, name)
+                test_function = make_json_test('name', dir_path, name)
+                setattr(TestsContainer, 'test_' + name, test_function)
             elif name == 'prov.rdf':
-                logger.validate_prov(dir_path, name)
-    if logger.fails:
-        PrettyPrinter(width=100).pprint(dict(logger.fails))
-        print('FAIL!')
-        exit(1)
-    else:
-        print('PASS!')
-
-
-if __name__ == '__main__':
-    main()
+                test_function = make_prov_test('name', dir_path, name)
+                setattr(TestsContainer, 'test_' + name, test_function)
+    unittest.main(verbosity=2)
