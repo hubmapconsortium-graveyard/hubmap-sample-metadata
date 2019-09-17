cbc0dd4a8ba8b2d7877905f0c3c7155655505f58
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-09-17 09:55:19 -0400

    Add "TestLogger"
    Use OO, instead of modifying arguments passed to function. Fix #23.

--- test.py
+++ test.py
@@ -23,54 +23,57 @@ def download_to(url, target):
     os.rename(download_path, target)
 
 
-def validate_json(dir_path, name, fails, hubmap_schema):
-    print(f'\tLoad JSON: {name}')
-    path = Path(dir_path) / name
-    metadata = json.load(open(path))
-    expected_suffix = metadata['schema_type'] + '.json'
-    if not name.endswith(expected_suffix):
-        fails[path]['suffix'] = f'Expected to end with "{expected_suffix}".'
-        return
-    described_by = metadata['describedBy']
-    schema_url = urlparse(described_by)
-    cache_path = f'schema-cache/{schema_url.hostname}{schema_url.path}'
-    if not os.path.isfile(cache_path):
-        download_to(described_by, cache_path)
-    schema = json.load(open(cache_path))
-    try:
-        validate(instance=metadata, schema=schema)
-    except (ValidationError, SchemaError) as e:
-        fails[path]['hca'] = e
-    try:
-        validate(instance=metadata, schema=hubmap_schema)
-    except (ValidationError, SchemaError) as e:
-        fails[path]['hubmap'] = e
+class TestLogger():
+    def __init__(self):
+        self.hubmap_schema = json.load(open('hubmap-schema.json'))
+        self.fails = defaultdict(dict)
 
+    def validate_json(self, dir_path, name):
+        print(f'\tLoad JSON: {name}')
+        path = Path(dir_path) / name
+        metadata = json.load(open(path))
+        expected_suffix = metadata['schema_type'] + '.json'
+        if not name.endswith(expected_suffix):
+            self.fails[path]['suffix'] = f'Expected to end with "{expected_suffix}".'
+            return
+        described_by = metadata['describedBy']
+        schema_url = urlparse(described_by)
+        cache_path = f'schema-cache/{schema_url.hostname}{schema_url.path}'
+        if not os.path.isfile(cache_path):
+            download_to(described_by, cache_path)
+        schema = json.load(open(cache_path))
+        try:
+            validate(instance=metadata, schema=schema)
+        except (ValidationError, SchemaError) as e:
+            self.fails[path]['hca'] = e
+        try:
+            validate(instance=metadata, schema=self.hubmap_schema)
+        except (ValidationError, SchemaError) as e:
+            self.fails[path]['hubmap'] = e
 
-def validate_prov(dir_path, name, fails):
-    print(f'\tLoad Provenance: {name}')
-    path = Path(dir_path) / name
-    provenance = prov.read(path, format='rdf')
-    output = StringIO()
-    serializer = prov.serializers.provn.ProvNSerializer(provenance)
-    serializer.serialize(output)
-    print(indent(output.getvalue(), '\t'))
+    def validate_prov(self, dir_path, name):
+        print(f'\tLoad Provenance: {name}')
+        path = Path(dir_path) / name
+        provenance = prov.read(path, format='rdf')
+        output = StringIO()
+        serializer = prov.serializers.provn.ProvNSerializer(provenance)
+        serializer.serialize(output)
+        print(indent(output.getvalue(), '\t'))
 
 
 def main():
-    fails = defaultdict(dict)
-    hubmap_schema = json.load(open('hubmap-schema.json'))
+    logger = TestLogger()
     for dir_path, _, file_names in os.walk('workflows'):
         print(f'Testing: {dir_path}')
         for name in sorted(file_names):
             if 'TODO' in name:
                 print(f'\tTODO: {name}')
             elif name.endswith('.json'):
-                validate_json(dir_path, name, fails, hubmap_schema)
+                logger.validate_json(dir_path, name)
             elif name == 'prov.rdf':
-                validate_prov(dir_path, name, fails)
-    if fails:
-        PrettyPrinter(width=100).pprint(dict(fails))
+                logger.validate_prov(dir_path, name)
+    if logger.fails:
+        PrettyPrinter(width=100).pprint(dict(logger.fails))
         print('FAIL!')
         exit(1)
     else:
